e298fa3208c9df25f77400cdf8fcdd21560dca31
Ivan Brown <ivan.lee.brown@gmail.com>
2013-04-29 21:00:11 -0500

    Event calendar JS  in-progress

---
+++ PhotoGallery/Scripts/eventCalendar/eventCalendar.js
@@ -0,0 +1,33 @@
+﻿
+EventCalendar = EventCalendar || {};
+
+(function ($, backbone, handlebars) {
+    EventCalendar.MonthCalendarModel = backbone.Model.extend({
+        url: '/Event/EventCalendar'        
+    });
+
+    EventCalendar.CalendarView = backbone.View.extend({
+        events: {
+            
+        },
+        initialize: function () {
+            var self = this;
+
+            self.template = handlebars.compile('../eventCalendar.html');
+
+            self.model = new EventCalendar.MonthCalendarModel();
+            self.model.bind('change', self.render);
+            self.model.idAttribute = new Date().toDateString();
+            self.model.fetch();
+
+            return self;
+        },
+        render: function () {
+            var self = this;
+
+            self.$el.empty().html(self.template.render(self.model.toJSON()));
+
+            return self;
+        }
+    });
+}(jQuery, Backbone, Handlebars));
