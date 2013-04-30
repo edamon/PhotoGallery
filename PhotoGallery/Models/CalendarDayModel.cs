fdbc004d75a9652a61a5ac90763472dbf126e0bb
Eric Damon <eric.damon@gmail.com>
2013-04-29 20:19:32 -0500

    add c# models for calendar

---
+++ PhotoGallery/Models/CalendarDayModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoGallery.Models
+{
+    public class CalendarDayModel
+    {
+        public DateTime Date { get; set; }
+        public string DayOfWeek { get; set; }
+        public bool IsInCurrentMonth { get; set; }
+        public bool HasEvents { get; set; }
+
+        public IEnumerable<EventModel> Events { get; set; }
+    }
+}
---
+++ PhotoGallery/Models/MonthCalendarModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoGallery.Models
+{
+    public class MonthCalendarModel
+    {
+        public DateTime CurrentMonth { get; set; }
+        public DateTime PreviousMonth { get; set; }
+        public DateTime NextMonth { get; set; }
+
+        public IEnumerable<CalendarDayModel> CalendarDays { get; set; }
+    }
+}
